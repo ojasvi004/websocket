9a37e5ab9fd13cc3539860bb94f18e5e71bb360b
Ojasvi <ojasvidoye@gmail.com>
2024-10-03 16:16:43 +0530

    add middleware

--- client/script.js
+++ client/script.js
@@ -7,10 +7,16 @@ const roomInput = document.getElementById("room-input");
 const form = document.getElementById("form");
 
 const socket = io("http://localhost:3000");
-const userSocket = io("http://localhost:3000/user");
+const userSocket = io("http://localhost:3000/user", {
+  auth: { token: "test" },
+});
 
 socket.on("connect", () => {
   displayMessage(`you connected with id: ${socket.id}`);
+});
+
+userSocket.on("connect_error", (error) => {
+  displayMessage(error);
 });
 
 socket.on("receive-message", (message) => {
--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
-const {instrument} = require('@socket.io/admin-ui')
+const { instrument } = require("@socket.io/admin-ui");
 const io = require("socket.io")(3000, {
   cors: {
     origin: ["http://localhost:8080", "https://admin.socket.io"],
   },
 });
 
-const userIo = io.of('/user')
-userIo.on('connection', socket => {
-  console.log('connected to user namespace')
-})
+const userIo = io.of("/user");
+userIo.on("connection", (socket) => {
+  console.log("connected to user namespace");
+  console.log("connected to user namespace with username" + socket.username);
+});
 
+userIo.use((socket, next) => {
+  if (socket.handshake.auth.token) {
+    socket.username = getUsernameFromToken(socket.handshake.auth.token);
+    next();
+  } else {
+    next(new Error("send token"));
+  }
+});
+
+function getUsernameFromToken(token) {
+  return token;
+}
 io.on("connection", (socket) => {
   console.log(socket.id);
   socket.on("send-message", (message, room) => {
@@ -25,6 +38,5 @@ io.on("connection", (socket) => {
     cb(`joined ${room}`);
   });
 });
- 
 
-instrument(io, {auth: false})
+instrument(io, { auth: false });
